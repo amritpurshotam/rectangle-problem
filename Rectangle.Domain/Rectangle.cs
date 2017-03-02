d744a6e0f867eddfd4d34276b348621677313ee3
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-02 20:41:39 +0200

    Add Rectangle class.

---
+++ Rectangle.Domain/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain
+{
+    public class Rectangle
+    {
+        public Rectangle(decimal length, decimal width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        public decimal Length { get; private set; }
+        public decimal Width { get; private set; }
+    }
+}
