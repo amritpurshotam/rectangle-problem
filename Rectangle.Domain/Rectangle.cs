178569ce537d56b37475c27721752d8b640d1b85
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-03 17:55:30 +0200

    Rename refactor length to height since it's more descriptive.

--- Rectangle.Domain/Rectangle.cs
+++ Rectangle.Domain/Rectangle.cs
@@ -2,14 +2,14 @@
 {
     public class Rectangle
     {
-        public Rectangle(Coordinate bottomLeftCoordinate, decimal length, decimal width)
+        public Rectangle(Coordinate bottomLeftCoordinate, decimal height, decimal width)
         {
             this.BottomLeftCoordinate = bottomLeftCoordinate;
-            this.Length = length;
+            this.Height = height;
             this.Width = width;
         }
 
-        public decimal Length { get; private set; }
+        public decimal Height { get; private set; }
         public decimal Width { get; private set; }
 
         public Coordinate BottomLeftCoordinate { get; private set; }
@@ -21,12 +21,12 @@
 
         public Coordinate TopLeftCoordinate
         {
-            get { return new Coordinate(BottomLeftCoordinate.X, BottomLeftCoordinate.Y + this.Length); }
+            get { return new Coordinate(BottomLeftCoordinate.X, BottomLeftCoordinate.Y + this.Height); }
         }
 
         public Coordinate TopRightCoordinate
         {
-            get { return new Coordinate(BottomLeftCoordinate.X + this.Width, BottomLeftCoordinate.Y + this.Length); }
+            get { return new Coordinate(BottomLeftCoordinate.X + this.Width, BottomLeftCoordinate.Y + this.Height); }
         }
     }
 }
