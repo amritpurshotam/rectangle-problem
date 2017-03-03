86102fcfae9a6ab92fbb39a48842efa69a1cddd6
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-03 17:47:00 +0200

    Add Coordinate class.

---
+++ Rectangle.Domain/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain
+{
+    public class Coordinate
+    {
+        public Coordinate(decimal x, decimal y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public decimal X { get; private set; }
+        public decimal Y { get; private set; }
+    }
+}
