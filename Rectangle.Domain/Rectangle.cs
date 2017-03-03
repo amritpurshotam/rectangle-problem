eb2df9c3190c2753ddbf17d740853e04192d3bdd
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-03 17:54:12 +0200

    Add Coordinates to Rectangle class.

--- Rectangle.Domain/Rectangle.cs
+++ Rectangle.Domain/Rectangle.cs
@@ -2,13 +2,31 @@
 {
     public class Rectangle
     {
-        public Rectangle(decimal length, decimal width)
+        public Rectangle(Coordinate bottomLeftCoordinate, decimal length, decimal width)
         {
+            this.BottomLeftCoordinate = bottomLeftCoordinate;
             this.Length = length;
             this.Width = width;
         }
 
         public decimal Length { get; private set; }
         public decimal Width { get; private set; }
+
+        public Coordinate BottomLeftCoordinate { get; private set; }
+
+        public Coordinate BottomRightCoordinate
+        {
+            get { return new Coordinate(BottomLeftCoordinate.X + this.Width, BottomLeftCoordinate.Y); }
+        }
+
+        public Coordinate TopLeftCoordinate
+        {
+            get { return new Coordinate(BottomLeftCoordinate.X, BottomLeftCoordinate.Y + this.Length); }
+        }
+
+        public Coordinate TopRightCoordinate
+        {
+            get { return new Coordinate(BottomLeftCoordinate.X + this.Width, BottomLeftCoordinate.Y + this.Length); }
+        }
     }
 }
