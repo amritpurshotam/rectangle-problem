8f67ec592d7d4162e57080fcb4684bccbe15003f
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-04 09:16:30 +0200

    Add unit test to test the number of rectangles generated.

---
+++ Rectangle.DomainLogic.Tests/Grid/GridServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Rectangle.DomainLogic.Services.Implementations;
+using Rectangle.DomainLogic.Services.Interfaces;
+
+namespace Rectangle.DomainLogic.Tests.Grid
+{
+    [TestFixture]
+    internal class GridServiceTest
+    {
+        private IGridService gridService;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.gridService = new GridService();
+        }
+
+        [Test]
+        public void GivenNumberOfRectanglesToGenerate_WhenInitialisingGrid_ThenGridMustHaveTheSpecifiedNumberOfRectangles()
+        {
+            const byte numberOfRectanglesToGenerate = 5;
+            var grid = this.gridService.InitialiseGrid(numberOfRectanglesToGenerate);
+            Assert.AreEqual(5, grid.RectangleList.Count);
+        }
+    }
+}
