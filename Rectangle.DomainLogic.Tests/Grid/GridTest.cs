383695fe77e6085bc3e9b5714aa406265c3a27e5
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-04 09:35:10 +0200

    Add initial Grid tests.

---
+++ Rectangle.DomainLogic.Tests/Grid/GridTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Rectangle.DomainLogic.Tests.Rectangle;
+
+namespace Rectangle.DomainLogic.Tests.Grid
+{
+    [TestFixture]
+    internal class GridTest
+    {
+        [Test]
+        public void Given_AGrid_When_AddingARectangle_Then_RectangleListMustHaveOneMoreRectangle()
+        {
+            var grid = new Domain.Grid();
+
+            grid.AddRectangle(RectangleStubs.RectangleStub);
+
+            Assert.AreEqual(1, grid.RectangleList.Count);
+        }
+
+        [Test]
+        public void Given_AGrid_When_GettingNextBottomLeftCoordinateAndRectangleListEmpty_Then_UseMinimumOfCoordinatesOfZeroZero()
+        {
+            var grid = new Domain.Grid();
+
+            var coordinate = grid.GetNextBottomLeftCoordinate();
+            
+            Assert.AreEqual(0, coordinate.X);
+            Assert.AreEqual(0, coordinate.Y);
+        }
+
+        [Test]
+        public void Given_AGrid_WhenGettingNextBottomLeftCoordinateAndThereAreSomeRectangles_Then_UseTheLastBottomRightCoordinate()
+        {
+            var grid = new Domain.Grid();
+            grid.AddRectangle(RectangleStubs.RectangleStub);
+
+            var coordinate = grid.GetNextBottomLeftCoordinate();
+
+            Assert.AreEqual(RectangleStubs.RectangleStub.BottomRightCoordinate.X, coordinate.X);
+            Assert.AreEqual(RectangleStubs.RectangleStub.BottomRightCoordinate.Y, coordinate.Y);
+        }
+    }
+}
---
+++ Rectangle.DomainLogic.Tests/Rectangle/RectangleStubs.cs
@@ -0,0 +1,15 @@
+﻿using Rectangle.Domain;
+
+namespace Rectangle.DomainLogic.Tests.Rectangle
+{
+    internal static class RectangleStubs
+    {
+        internal static Domain.Rectangle RectangleStub
+        {
+            get
+            {
+                return new Domain.Rectangle(new Coordinate(0,0), 3, 5);
+            }
+        }
+    }
+}
