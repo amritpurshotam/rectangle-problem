28e31cb7d841ddfa955933b7276b70f96dd9edd6
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-04 13:40:40 +0200

    Add initial rectangle controller and view.

---
+++ Rectangle.Web/Controllers/RectangleController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace RectangleProblem.Controllers
+{
+    public class RectangleController : Controller
+    {
+        public const string RectangleControllerName = "Rectangle";
+
+        public const string GenerateActionName = "Generate";
+        [HttpGet]
+        public ActionResult Generate()
+        {
+            return View();
+        }
+	}
+}
