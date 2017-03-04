7bd4f277ba35b6fda189166c871011f8e9a93ef0
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-04 08:38:11 +0200

    Add initialise grid method.

---
+++ Rectangle.DomainLogic/Services/Implementations/GridService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Rectangle.Domain;
+using Rectangle.DomainLogic.Services.Interfaces;
+
+namespace Rectangle.DomainLogic.Services.Implementations
+{
+    public class GridService : IGridService
+    {
+        public GridService()
+        {
+        }
+
+        public Grid InitialiseGrid(byte number)
+        {
+            var grid = new Grid();
+
+            var random = new Random();
+            for (var i = 0; i < number; i++)
+            {
+                var height = random.Next();
+                var width = random.Next();
+
+                var bottomLeftCoordinate = grid.GetNextBottomLeftCoordinate();
+                var rectangle = new Domain.Rectangle(bottomLeftCoordinate, height, width);
+                
+                grid.AddRectangle(rectangle);
+            }
+
+            return grid;
+        } 
+    }
+}
---
+++ Rectangle.DomainLogic/Services/Interfaces/IGridService.cs
@@ -0,0 +1,9 @@
+﻿using Rectangle.Domain;
+
+namespace Rectangle.DomainLogic.Services.Interfaces
+{
+    public interface IGridService
+    {
+        Grid InitialiseGrid(byte number);
+    }
+}
