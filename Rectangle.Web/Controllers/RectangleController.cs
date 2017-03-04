41b16927bb16ceff78044d813d5ae99e4c14c3c3
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-04 13:57:42 +0200

    Generate a grid and let the user download a text file with the rectangle's dimensions.

--- Rectangle.Web/Controllers/RectangleController.cs
+++ Rectangle.Web/Controllers/RectangleController.cs
@@ -1,16 +1,33 @@
-﻿using System.Web.Mvc;
+﻿using System.Text;
+using System.Web.Mvc;
+using Rectangle.DomainLogic.Services.Interfaces;
+using RectangleProblem.Models.Rectangle;
 
 namespace RectangleProblem.Controllers
 {
     public class RectangleController : Controller
     {
         public const string RectangleControllerName = "Rectangle";
+        private readonly IGridService gridService;
+
+        public RectangleController(IGridService gridService)
+        {
+            this.gridService = gridService;
+        }
 
         public const string GenerateActionName = "Generate";
         [HttpGet]
         public ActionResult Generate()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Download(GenerateRectangleInput model)
+        {
+            var grid = gridService.InitialiseWithRectanglesOfRandomSize(model.NumberOfRectangles);
+            return File(Encoding.UTF8.GetBytes(grid.ToStringUsingDimensionsOfRectangles()), "text/plain");
         }
 	}
 }
---
+++ Rectangle.Web/Models/Rectangle/GenerateRectangleInput.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RectangleProblem.Models.Rectangle
+{
+    public class GenerateRectangleInput
+    {
+        [Range(3, 30, ErrorMessage = "Number of rectangles must be between 3 and 30 inclusive.")]
+        public byte NumberOfRectangles { get; set; }
+    }
+}
