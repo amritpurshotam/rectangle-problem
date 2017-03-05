433925a25e3c6b00ea592c05731039fac0567f8b
amrit.purshotam <amrit.purshotam@entelect.co.za>
2017-03-05 14:50:01 +0200

    Display rectangles in different colours.

--- Rectangle.Web/Scripts/PageScripts/rectangle.js
+++ Rectangle.Web/Scripts/PageScripts/rectangle.js
@@ -8,6 +8,10 @@
             var bottomLeftCoordinate = grid.RectangleList[i].BottomLeftCoordinate;
             var width = grid.RectangleList[i].Width;
             var height = grid.RectangleList[i].Height;
+
+            var colour = randomColor({ luminosity: 'light', count: 1 });
+            context.fillStyle = colour;
+
             context.fillRect(bottomLeftCoordinate.X, -bottomLeftCoordinate.Y, width, -height);
         }
     }
